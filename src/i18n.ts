5a973032c74ab9efca62679d9e5d0a54825fb86f
Carlos Dugarte <soycarlosdugarte@gmail.com>
2025-03-24 15:56:30 -0300

    fix vulnerability to next and fix getrequestconfig

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,10 +2,14 @@ import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 import { locales } from './i18n/config';
 
-export default getRequestConfig(async ({ locale }) => {
-  if (!locales.includes(locale)) notFound();
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = await requestLocale; // Obtiene el idioma dinámicamente
+
+  // Verifica si el locale es válido
+  if (!locale || !locales.includes(locale)) notFound();
 
   return {
+    locale, // Incluye el locale en el objeto retornado
     messages: (await import(`./i18n/locales/${locale}/globals.json`)).default,
   };
 });
